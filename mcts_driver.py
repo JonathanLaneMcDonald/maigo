45df28b8f14b84e3f3d382bf95439ed9bdc31ac6
Lane McDonald <Jonathan.Lane.McDonald@gmail.com>
2021-10-04 20:53:46 +1100

    a really simple model should be sufficient

---
+++ mcts_driver.py
@@ -0,0 +1,15 @@
+
+"""
+i'll use this script to try and get a model, a tree search, and a game working together
+"""
+
+
+
+
+
+
+
+
+
+
+
--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 
 from keras.models import Model
-from keras.layers import Input, BatchNormalization, Convolution2D, Activation, Add, Dense, Flatten, Dropout
+from keras.layers import Input, BatchNormalization, Convolution2D, Activation, Add, Dense, Flatten
 from keras.optimizers import Adam
 from keras.initializers import RandomNormal
 
@@ -12,7 +12,7 @@ def add_bn_relu_convolve_block(x, filters, kernel_size=(3, 3)):
 	return x
 
 
-def build_agz_model(blocks, filters, input_shape):
+def build_agz_model(blocks, filters, input_shape, policy_options):
 
 	inputs = Input(shape=input_shape)
 
@@ -26,7 +26,7 @@ def build_agz_model(blocks, filters, input_shape):
 	policy = x
 	policy = add_bn_relu_convolve_block(policy, 2, (1, 1))
 	policy = Flatten()(policy)
-	policy = Dense(input_shape[0]*input_shape[1]+1, activation='softmax', name='policy')(policy)
+	policy = Dense(policy_options, activation='softmax', name='policy')(policy)
 
 	value = x
 	value = add_bn_relu_convolve_block(value, 1, (1, 1))
@@ -46,29 +46,3 @@ def build_agz_model(blocks, filters, input_shape):
 	model.summary()
 
 	return model
-
-
-def build_rollout_policy(blocks, filters, input_shape):
-
-	inputs = Input(shape=input_shape)
-
-	x = inputs
-	for block in range(blocks):
-		x = Convolution2D(filters=filters, kernel_size=(3, 3), padding='same', activation='relu')(x)
-		x = BatchNormalization()(x)
-
-	x = Convolution2D(filters=1, kernel_size=(1, 1), padding='same', activation='relu')(x)
-
-	x = Flatten()(x)
-
-	outputs = Dense(input_shape[0]*input_shape[1]+1, activation='softmax')(x)
-
-	model = Model(inputs, outputs)
-	model.compile(loss='sparse_categorical_crossentropy', optimizer=Adam(), metrics=["accuracy"])
-	model.summary()
-
-	return model
-
-
-
-
