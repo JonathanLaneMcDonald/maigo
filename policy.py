33e7c61de3d198dbde384e97ba3d6a4039b50efd
Lane McDonald <Jonathan.Lane.McDonald@gmail.com>
2021-10-09 14:27:56 +1100

    prep for mcts and model-based mcts by creating a policy interface

---
+++ policy.py
@@ -0,0 +1,16 @@
+
+class Policy:
+	"""
+	Policy interface is implemented by any distribution that generates moves:
+		human beans,
+		random samplers,
+		raw or weighted model outputs,
+		vanilla mcts,
+		model-enabled mcts,
+		etc.
+	"""
+
+	...
+
+
+
